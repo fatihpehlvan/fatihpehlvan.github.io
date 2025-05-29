c7e6b138129fec9ecaeb34a2d005392c6c3804fa
Fatih Pehlivan <fpehlivan1542@gmail.com>
2025-05-30 00:47:59 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
+  assetsInclude: ['**/*.jsx']
 })
